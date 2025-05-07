ac74d269827741ff4109a3ae38c2cf60703f3551
isabella <isabella.pizani.galvao@escola.pr.gov.br>
2025-05-07 10:56:27 -0300

    Auto stash before rebase of "refs/heads/main"
    significado de expressoes

---
+++ pythonProject/capitulo 2/exemplo3expressoeseinstrucoes.py
@@ -0,0 +1,8 @@
+#variaveis nao podem começar com numeros
+## 76trombones = 'big parade'
+
+#variaveis nao podem comecar com letra maiuscula
+# more@ = 1000000
+
+#variaveis nao podem ter caracteres especiais
+#tirando o _ underline
