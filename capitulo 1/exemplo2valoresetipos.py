271c2b0bad827853a5031eb7578993a225fc6ba8
isabella <isabella.pizani.galvao@escola.pr.gov.br>
2025-05-20 11:45:13 -0300

    exemplo  2 valor e tipo

---
+++ capitulo 1/exemplo2valoresetipos.py
@@ -0,0 +1,21 @@
+# tipo inteiro 1,2,3.....-1,-2,-3,-4...
+inteiro = 2
+
+print(type(inteiro))
+
+# tipo real ou ponto flutuantes float 1.3 , 0.6....-3.456
+
+float = 1.1
+print(type(float))
+
+# characteres
+char1 = 'a'
+char2 = '2'
+
+print(type(char1))
+
+#string conjunto de caracteres
+
+StringNome = ("jose valim")
+
+print(type(StringNome))
