952a5872405ebca574b56610002d55526e137f93
isabella <isabella.pizani.galvao@escola.pr.gov.br>
2025-06-17 11:50:45 -0300

    exemplo 2

---
+++ capitulo 3/exemplo exercico 1.py
@@ -0,0 +1,24 @@
+def adicao(numero, numero2):
+    return numero + numero2
+
+def subtracao(numero)
+    return numero - numero2
+
+def multiplicacao(numero1, numero2):
+    return numero1 * numero2
+
+def divisao(numero1, numero2):
+    return numero1 / numero2
+#main
+numero = int(input("digite o primeiro numero "))
+numero2 = int(input("digite o segundo numero"))
+
+resultadoAdicao = adicao(numero , numero2) # as variaveis sao argumentos da
+resultadosubtracao = subtracao(numero , numero2)
+resultadoMultiplicaco = multiplicacao(numero1, numero2)
+resultadodivisao = divisao(numero1, numero2)
+
+print("O resultado da função soma", resultadoSoma)
+print("O resultado da função subtracao",resultadoSubtracao)
+print("O resultado da função multiplicação",resultadoMultiplicaco)
+print("O resultado da função divisao",resultadodivisao)
