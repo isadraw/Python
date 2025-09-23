8c46e10915bc155b8c7deec1ac0bcf6f4f790968
isabella <isabella.pizani.galvao@ecola.r.gov.br>
2025-09-23 11:52:12 -0300

    exemplo

---
+++ capitulo 5/atividade.py
@@ -0,0 +1,9 @@
+def apresentacao(nome,nomemae,nomepai):
+    print(f"nome:{nome}")
+    print(f"nome da mae:{nomemae}")
+    print(f"nome do pai:{nomepai}")
+nome= input("digite seu nome")
+nomemae = input("digite o nome da sua mãe")
+nomepai =input("digite o nome de seu pai")
+
+apresentacao(nome ,nomemae,nomepai)
